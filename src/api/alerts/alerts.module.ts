2fdb949ff3acf4954ef10afe4b498662970dd265
Guilherme Alexandre <guilherme_alexandree@hotmail.com>
2025-01-24 00:08:38 -0300

    refactor: consolidate use case imports in alerts.module.ts

--- src/api/alerts/alerts.module.ts
+++ src/api/alerts/alerts.module.ts
@@ -2,15 +2,11 @@ import { Module } from '@nestjs/common';
 import { AlertsService } from './alerts.service';
 import { AlertsController } from './alerts.controller';
 import { AlertsRepository } from './alerts.repository';
-import { SeedAlertsUseCase } from './usecases/seed-alerts-use-case';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Alert } from './entities/alert.entity';
 import { CompaniesModule } from '../companies/companies.module';
 import { DateAdapterModule } from '../../infra/adapters/date-adapter';
-import { GetAllAlertsUseCase } from './usecases/get-all-alerts-use-case';
-import { CreateAlertUseCase } from './usecases/create-alert-use-case';
-import { UpdateAlertUseCase } from './usecases/update-alert-use-case';
-import { RemoveAlertUseCase } from './usecases/remove-alert-use-case';
+import { CreateAlertUseCase, GetAllAlertsUseCase, RemoveAlertUseCase, SeedAlertsUseCase, UpdateAlertUseCase } from '@/api/alerts/usecases';
 
 @Module({
   imports: [
