9c2069399aa3e4c46172b6f50564bb8f27bd0bda
Guilherme Alexandre <guilherme_alexandree@hotmail.com>
2025-01-24 20:00:40 -0300

    fix: update import path in main.ts to remove TypeScript extension

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import 'module-alias/register'
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module.ts';
+import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import morgan from 'morgan';
