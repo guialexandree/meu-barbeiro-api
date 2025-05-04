a865ea7497dbbffcad6aea81caa969a99a8e9e6f
Guilherme Alexandre <guilherme_alexandree@hotmail.com>
2025-05-04 03:39:44 -0300

    fix: update Swagger module setup path to root for improved accessibility

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build()
   const documentFactory = () => SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api/swagger', app, documentFactory)
+  SwaggerModule.setup('/', app, documentFactory)
 
   await app.listen(process.env.PORT ?? 3000)
 }
