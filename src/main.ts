d239e6c7c8017c9477e56f518a6a4e6c8af24d48
Guilherme Alexandre <guilherme_alexandree@hotmail.com>
2025-05-12 16:15:00 -0300

    feat: add morgan middleware for enhanced logging in development

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { ValidationPipe } from '@nestjs/common'
 import { HttpDefautResponseMiddleware, HttpExceptionResponse } from './infra/middlewares'
+import morgan = require('morgan')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true })
 
+  app.use(morgan('dev'))
   app.setGlobalPrefix('api')
   app.useGlobalPipes(new ValidationPipe())
   app.useGlobalFilters(new HttpExceptionResponse())
