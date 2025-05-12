9a426caad9d4fbcbc24e18a561f0a91198c15ef3
Guilherme Alexandre <guilherme_alexandree@hotmail.com>
2025-05-12 19:26:00 -0300

    fix: correct spelling in API description in main.ts

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('Barbearia API')
-    .setDescription('API de gereciamento do sistema para barbearias')
+    .setDescription('AsPI de gereciamento do sistema para barbearias')
     .addSecurity('bearer', {
       type: 'http',
       scheme: 'bearer',
