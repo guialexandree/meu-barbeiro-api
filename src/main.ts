4850c373a3d99b529b93cfc10a6a730654c6be4a
Guilherme Alexandre <guilherme_alexandree@hotmail.com>
2025-05-11 22:29:52 -0300

    feat: set global prefix for API routes

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import { HttpDefautResponseMiddleware, HttpExceptionResponse } from './infra/mid
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true })
 
-  app.setGlobalPrefix('api')
   app.useGlobalPipes(new ValidationPipe())
   app.useGlobalFilters(new HttpExceptionResponse())
   app.useGlobalInterceptors(new HttpDefautResponseMiddleware())
